055c6419199183d5d2101ee966ff4c0497119f7d
Victor Kamei Kay <kameikay@gmail.com>
2023-08-27 21:14:34 -0300

    [feat]: add absolute paths

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,21 @@
-module.exports = function(api) {
+// eslint-disable-next-line no-undef
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          root: ".",
+          extensions: [".ios.js", ".android.js", ".js", ".ts", ".tsx", ".jsx", ".json"],
+          alias: {
+            "@assets": "./src/assets",
+            "@components": "./src/components",
+            "@styles": "./src/styles",
+          },
+        },
+      ],
+    ],
   };
 };
