a0cbe619e951a4912b5116dc9d5aa70cc8e0a0b9
Victor Kamei Kay <kameikay@gmail.com>
2023-08-29 00:09:35 -0300

    [feat]: add react-hook-form lib

--- babel.config.js
+++ babel.config.js
@@ -19,6 +19,7 @@ module.exports = function (api) {
             "@services": "./src/services",
             "@styles": "./src/styles",
             "@utils": "./src/utils",
+            "@customTypes": "./src/types",
           },
         },
       ],
