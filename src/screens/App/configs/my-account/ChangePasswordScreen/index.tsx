ecd2e449ee52362945f4aeec96a5b8da65943734
Victor Kamei Kay <kameikay@gmail.com>
2023-09-13 22:49:01 -0300

    [feat]: add configs - change password file

---
+++ src/screens/App/configs/my-account/ChangePasswordScreen/index.tsx
@@ -0,0 +1,10 @@
+import { Text } from "@components/Text";
+import { Container } from "./styles";
+
+export default function PersonalDataScreen() {
+  return (
+    <Container>
+      <Text>Alterar senha</Text>
+    </Container>
+  );
+}
