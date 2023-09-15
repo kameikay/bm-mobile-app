733866c7bd9200527ce18d86d30f3ea02c6b662e
Victor Kamei Kay <kameikay@gmail.com>
2023-09-14 23:16:43 -0300

    [feat]: add change password page

---
+++ src/screens/App/configs/styles.ts
@@ -0,0 +1,16 @@
+import styled from "styled-components/native";
+
+export const Container = styled.SafeAreaView`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+  padding-top: 16px;
+`;
+
+export const TextListContainer = styled.TouchableOpacity`
+  width: 100%;
+  padding: 16px 8px;
+  justify-content: space-between;
+  flex-direction: row;
+  align-items: center;
+`;
--- src/screens/App/home/index.tsx
+++ src/screens/App/home/index.tsx
@@ -1,33 +1,9 @@
-import { ActivityIndicator, Image, Platform } from "react-native";
+import { Image, Platform } from "react-native";
 
 import { Text } from "@components/Text";
 import { Container, HeaderContainer, LoginContainer } from "./styles";
-import { useQuery } from "react-query";
-import UserService from "@services/UserService";
-import { theme } from "@styles/theme/default";
-import { useEffect } from "react";
-import { useSelfDataStore } from "src/store/useSelfData";
 
 export default function HomeScreen() {
-  const { data, isLoading } = useQuery("selfData", () =>
-    UserService.getPersonalData()
-  );
-  const { setMe } = useSelfDataStore();
-
-  if (isLoading) {
-    return (
-      <Container>
-        <ActivityIndicator size="large" color={theme.colors.primary[400]} />
-      </Container>
-    );
-  }
-
-  useEffect(() => {
-    if (data && data.data) {
-      setMe(data.data.data);
-    }
-  }, [data]);
-
   return (
     <Container>
       <LoginContainer
