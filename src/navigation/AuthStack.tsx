6764a7a360ffa33d63e5ea3d87df2e219a3bd85d
Victor Kamei Kay <kameikay@gmail.com>
2023-09-14 21:29:09 -0300

    [fix]: auth stack

--- src/navigation/AuthStack.tsx
+++ src/navigation/AuthStack.tsx
@@ -2,22 +2,22 @@ import {
   NativeStackNavigationProp,
   createNativeStackNavigator,
 } from "@react-navigation/native-stack";
-import ConfirmationCodeScreen from "@screens/Auth/ConfirmationCodeScreen";
+import ConfirmationCode from "@screens/Auth/ConfirmationCodeScreen";
 import LoginScreen from "@screens/Auth/LoginScreen";
 import SignUpScreen from "@screens/Auth/SignUpScreen";
 import { theme } from "@styles/theme/default";
 
-const Stack = createNativeStackNavigator();
-
-type AuthStackParamList = {
+export type AuthStackParamList = {
   Login: undefined;
   SignUp: undefined;
   ConfirmationCode: {
-    email: string;
+    email: string | null;
   };
 };
 
 export type AuthStackTypes = NativeStackNavigationProp<AuthStackParamList>;
+
+const Stack = createNativeStackNavigator<AuthStackParamList>();
 
 export default function AuthStack() {
   return (
@@ -28,13 +28,16 @@ export default function AuthStack() {
           backgroundColor: theme.colors.gray[50],
         },
       }}
-      initialRouteName="Login"
     >
       <Stack.Screen name="Login" component={LoginScreen} />
       <Stack.Screen name="SignUp" component={SignUpScreen} />
       <Stack.Screen
         name="ConfirmationCode"
-        component={ConfirmationCodeScreen}
+        component={ConfirmationCode}
+        initialParams={{
+          email: "",
+        }}
+        navigationKey="ConfirmationCode"
       />
     </Stack.Navigator>
   );
